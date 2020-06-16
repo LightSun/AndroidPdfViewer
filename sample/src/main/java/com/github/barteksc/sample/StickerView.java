377655787a2d307e56e5fd438980da4865eb1ed3
heaven7 <18111646505@163.com>
2020-06-16 18:28:45 +0800

    opt

--- sample/src/main/java/com/github/barteksc/sample/StickerView.java
+++ sample/src/main/java/com/github/barteksc/sample/StickerView.java
@@ -7,7 +7,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
@@ -17,6 +16,7 @@ import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 
 import com.heaven7.core.util.Logger;
 
@@ -47,9 +47,7 @@ public class StickerView extends View {
 
     private Bitmap mSticker;
     private OnClickTextListener mOnClickTextListener;
-    //content margin top and bottom
-    private int mMarginStart;
-    private int mMarginTop;
+
 
     public StickerView(Context context) {
         this(context, null);
@@ -71,6 +69,14 @@ public class StickerView extends View {
         mEffect = new DashPathEffect(new float[]{mParams.pe_interval, mParams.pe_interval}, mParams.pe_phase);
 
         mGestureDetector = new GestureDetectorCompat(context, new Gesture0());
+        getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                getViewTreeObserver().removeOnPreDrawListener(this);
+                adjustMargin();
+                return true;
+            }
+        });
     }
 
     public void setOnClickTextListener(OnClickTextListener onClickTextListener) {
@@ -78,16 +84,18 @@ public class StickerView extends View {
     }
 
     public int getMarginStart() {
-        return mMarginStart;
+        return mParams.marginStart;
     }
     public void setMarginStart(int mMarginStart) {
-        this.mMarginStart = mMarginStart;
+        mParams.marginStart = mMarginStart;
+        invalidate();
     }
     public int getMarginTop() {
-        return mMarginTop;
+        return mParams.marginTop;
     }
     public void setMarginTop(int mMarginTop) {
-        this.mMarginTop = mMarginTop;
+        mParams.marginTop = mMarginTop;
+        invalidate();
     }
 
     public void setSticker(int drawableId){
@@ -138,7 +146,7 @@ public class StickerView extends View {
         final int stickerHeight = mParams.stickerHeight <=0 ? mSticker.getHeight() : mParams.stickerHeight;
 
         canvas.save();
-        canvas.translate(paddingStart + mMarginStart, paddingTop + mMarginTop);
+        canvas.translate(paddingStart + mParams.marginStart, paddingTop + mParams.marginTop);
         //draw sticker
         mRect.set(0, 0, mSticker.getWidth(), mSticker.getHeight());
         mRectF.set(0, 0, stickerWidth, stickerHeight);
@@ -162,32 +170,32 @@ public class StickerView extends View {
         canvas.drawCircle(stickerWidth, stickerHeight, mParams.dotRadius, mLinePaint);
         canvas.drawCircle(0, stickerHeight, mParams.dotRadius, mLinePaint);
         //---------- texts' --------------
-        //text prepare
-        mTextPaint.setStyle(Paint.Style.STROKE);
-        mTextPaint.setTextSize(mParams.textSize);
-        mTextPaint.getTextBounds(mParams.text, 0, mParams.text.length(), mRect);
-        final int textWidth = mRect.width();
-        final int textHeight = mRect.height();
-        int left = stickerWidth + mParams.textMarginStart;
-        int top = (stickerHeight - textHeight - mParams.textPaddingTop - mParams.textPaddingBottom) / 2;
-        int right = left + textWidth + mParams.textPaddingStart + mParams.textPaddingEnd;
-        int bottom = top + textHeight + mParams.textPaddingTop + mParams.textPaddingBottom;
-        //text bg
-        mTextPaint.setStyle(Paint.Style.FILL);
-        mTextPaint.setColor(mParams.textBgColor);
-        mRectF.set(left, top, right, bottom);
-        mTextArea.set(mRectF);
-        canvas.drawRoundRect(mRectF, mParams.textBgRoundSize, mParams.textBgRoundSize, mTextPaint);
-        //text
-        mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        mTextPaint.setColor(mParams.textColor);
+        if(mParams.textEnabled){
+            //text prepare
+            measureText0();
+            final int textWidth = mRect.width();
+            final int textHeight = mRect.height();
+            int left = stickerWidth + mParams.textMarginStart;
+            int top = (stickerHeight - textHeight - mParams.textPaddingTop - mParams.textPaddingBottom) / 2;
+            int right = left + textWidth + mParams.textPaddingStart + mParams.textPaddingEnd;
+            int bottom = top + textHeight + mParams.textPaddingTop + mParams.textPaddingBottom;
+            //text bg
+            mTextPaint.setStyle(Paint.Style.FILL);
+            mTextPaint.setColor(mParams.textBgColor);
+            mRectF.set(left, top, right, bottom);
+            mTextArea.set(mRectF);
+            canvas.drawRoundRect(mRectF, mParams.textBgRoundSize, mParams.textBgRoundSize, mTextPaint);
+            //text
+            mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+            mTextPaint.setColor(mParams.textColor);
 
-        mRect.set(left + mParams.textPaddingStart,
-                top + mParams.textPaddingTop,
-                left + mParams.textPaddingStart + textWidth,
-                top + mParams.textPaddingTop + textHeight);
-        DrawingUtils.computeTextDrawingCoordinate(mParams.text, mTextPaint, mRect, mRectF);
-        canvas.drawText(mParams.text, mRectF.left, mRectF.top - mTextPaint.ascent(), mTextPaint);
+            mRect.set(left + mParams.textPaddingStart,
+                    top + mParams.textPaddingTop,
+                    left + mParams.textPaddingStart + textWidth,
+                    top + mParams.textPaddingTop + textHeight);
+            DrawingUtils.computeTextDrawingCoordinate(mParams.text, mTextPaint, mRect, mRectF);
+            canvas.drawText(mParams.text, mRectF.left, mRectF.top - mTextPaint.ascent(), mTextPaint);
+        }
 
         canvas.restore();
     }
@@ -204,7 +212,7 @@ public class StickerView extends View {
      */
     private boolean containsInRect(RectF target, float x, float y, int growX, int growY) {
         mRectF.set(target);
-        mRectF.offset(getPaddingStart() + mMarginStart, getPaddingTop() + mMarginTop);
+        mRectF.offset(getPaddingStart() + mParams.marginStart, getPaddingTop() + mParams.marginTop);
         mRectF.set(mRectF.left - growX,
                 mRectF.top - growY,
                 mRectF.right + growX,
@@ -236,11 +244,33 @@ public class StickerView extends View {
         );
         return containsInRect(mRectF, e.getX(), e.getY(), GROWXY * 2, GROWXY * 2);
     }
-
-    private void move(int dx, int dy){
-        mMarginStart += dx;
-        mMarginTop += dy;
-        invalidate();
+    private void measureText0(){
+        mTextPaint.setStyle(Paint.Style.STROKE);
+        mTextPaint.setTextSize(mParams.textSize);
+        mTextPaint.getTextBounds(mParams.text, 0, mParams.text.length(), mRect);
+    }
+    private int getContentWidth(){
+        final int stickerWidth = mParams.stickerWidth <=0 ? mSticker.getWidth() : mParams.stickerWidth;
+        if(!mParams.textEnabled){
+            return stickerWidth;
+        }
+        measureText0();
+        final int textWidth = mRect.width();
+        return stickerWidth + mParams.textMarginStart + textWidth + mParams.textPaddingStart + mParams.textPaddingEnd;
+    }
+    private int getContentHeight(){
+        final int stickerHeight = mParams.stickerHeight <=0 ? mSticker.getHeight() : mParams.stickerHeight;
+        return stickerHeight;
+    }
+    private void adjustMargin(){
+        if(mParams.marginStart < 0){
+            mParams.marginStart = getWidth() - getContentWidth() - getPaddingStart()
+                    - getPaddingEnd() - Math.abs(mParams.marginStart);
+        }
+        if(mParams.marginTop < 0){
+            mParams.marginTop = getHeight() - getContentHeight() - getPaddingTop()
+                    - getPaddingBottom() - Math.abs(mParams.marginTop);
+        }
     }
 
     private class Gesture0 implements GestureDetector.OnGestureListener{
@@ -252,8 +282,8 @@ public class StickerView extends View {
         private int mTmpMarginStart;
 
         private void moveInternal(int dx, int dy){
-            mMarginStart = mTmpMarginStart + dx;
-            mMarginTop = mTmpMarginTop + dy;
+            mParams.marginStart = mTmpMarginStart + dx;
+            mParams.marginTop = mTmpMarginTop + dy;
             invalidate();
         }
 
@@ -264,8 +294,8 @@ public class StickerView extends View {
                 mTmpStickerWidth = mParams.stickerWidth > 0 ? mParams.stickerWidth : mSticker.getWidth();
                 mTmpStickerHeight = mParams.stickerHeight > 0 ? mParams.stickerHeight : mSticker.getHeight();
             }
-            mTmpMarginStart = mMarginStart;
-            mTmpMarginTop = mMarginTop;
+            mTmpMarginStart = mParams.marginStart;
+            mTmpMarginTop = mParams.marginTop;
             return true;
         }
         @Override
@@ -274,7 +304,7 @@ public class StickerView extends View {
         }
         @Override
         public boolean onSingleTapUp(MotionEvent e) {
-            if(containsInRect(mTextArea, e.getX(), e.getY(), GROWXY, GROWXY)){
+            if(mParams.textEnabled && containsInRect(mTextArea, e.getX(), e.getY(), GROWXY, GROWXY)){
                 if(mOnClickTextListener != null){
                     mOnClickTextListener.onClickTextArea(StickerView.this);
                 }
@@ -368,7 +398,13 @@ public class StickerView extends View {
         int textPaddingEnd;
         int textPaddingBottom;
         String text = "旋转";
+        boolean textEnabled;
+
         boolean proportionalZoom;
+
+        //content margin top and bottom
+        int marginStart = -1;
+        int marginTop = -1 ;
 
         public void init(TypedArray ta){
             stickerWidth = ta.getDimensionPixelOffset(R.styleable.StickerView_stv_sticker_width, 0);
@@ -381,6 +417,7 @@ public class StickerView extends View {
             dotRadius = ta.getDimensionPixelSize(R.styleable.StickerView_stv_dotRadius, 0);
             dotColor = ta.getColor(R.styleable.StickerView_stv_dotColor, Color.BLACK);
 
+            textEnabled = ta.getBoolean(R.styleable.StickerView_stv_text_enable, true);
             textBgColor = ta.getColor(R.styleable.StickerView_stv_text_bg_color, Color.BLACK);
             textBgRoundSize = ta.getDimensionPixelSize(R.styleable.StickerView_stv_text_bg_round, 20);
 
@@ -392,7 +429,10 @@ public class StickerView extends View {
             textPaddingTop = ta.getDimensionPixelOffset(R.styleable.StickerView_stv_text_padding_top, 0);
             textPaddingEnd = ta.getDimensionPixelOffset(R.styleable.StickerView_stv_text_padding_end, 0);
             textPaddingBottom = ta.getDimensionPixelOffset(R.styleable.StickerView_stv_text_padding_bottom, 0);
+
             proportionalZoom = ta.getBoolean(R.styleable.StickerView_stv_proportional_zoom, true);
+            marginStart = ta.getDimensionPixelSize(R.styleable.StickerView_stv_content_margin_start, 0);
+            marginTop = ta.getDimensionPixelSize(R.styleable.StickerView_stv_content_margin_top, 0);
         }
     }
 
